007b7fcca02e41e6cb35710d565b723522e4cd01
Biju Mondal <bijumondal18@gmail.com>
2023-06-03 10:33:48 +0530

    hide keyboard from support screen on outside click

--- lib/Features/Support/Presentation/support_screen.dart
+++ lib/Features/Support/Presentation/support_screen.dart
@@ -18,47 +18,53 @@ class _SupportScreenState extends State<SupportScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(AppSizes.kDefaultPadding),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(
-              height: AppSizes.kDefaultPadding,
-            ),
-            Text(
-              'Support',
-              style: Theme.of(context)
-                  .textTheme
-                  .headlineSmall!
-                  .copyWith(fontWeight: FontWeight.w400),
-            ),
-            const SizedBox(
-              height: AppSizes.kDefaultPadding * 1.2,
-            ),
-            CustomTextField(
-              controller: _subjectController,
-              hintText: 'Subject',
-              isBorder: false,
-              maxLines: 1,
-              minLines: 1,
-            ),
-            const SizedBox(
-              height: AppSizes.kDefaultPadding,
-            ),
-            CustomTextField(
-              isBorder: false,
-              controller: _messageController,
-              hintText: 'Message',
-              maxLines: 5,
-              minLines: 5,
-            ),
-            const SizedBox(
-              height: AppSizes.kDefaultPadding * 2,
-            ),
-            FullButton(label: 'Submit Query', onPressed: () {})
-          ],
+    return GestureDetector(
+      onTap: () {
+        //Hide the onscreen keyboard on outside touch.
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(AppSizes.kDefaultPadding),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: AppSizes.kDefaultPadding,
+              ),
+              Text(
+                'Support',
+                style: Theme.of(context)
+                    .textTheme
+                    .headlineSmall!
+                    .copyWith(fontWeight: FontWeight.w400),
+              ),
+              const SizedBox(
+                height: AppSizes.kDefaultPadding * 1.2,
+              ),
+              CustomTextField(
+                controller: _subjectController,
+                hintText: 'Subject',
+                isBorder: false,
+                maxLines: 1,
+                minLines: 1,
+              ),
+              const SizedBox(
+                height: AppSizes.kDefaultPadding,
+              ),
+              CustomTextField(
+                isBorder: false,
+                controller: _messageController,
+                hintText: 'Message',
+                maxLines: 5,
+                minLines: 5,
+              ),
+              const SizedBox(
+                height: AppSizes.kDefaultPadding * 2,
+              ),
+              FullButton(label: 'Submit Query', onPressed: () {})
+            ],
+          ),
         ),
       ),
     );
